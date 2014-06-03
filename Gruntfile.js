5127bbdbd364e0269eeadbb560d30654fb92d207
Frost <frost@domo.com>
2014-06-03 01:40:41 -0600

    initial commit for step 2 end

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,36 @@
 module.exports = function(grunt){
 
-  grunt.initConfig();
+  grunt.initConfig({
+    stylus:{
+      compile:{
+        options:{compress:false},
+        files:{
+          'css/app.css': 'styl/app.styl'
+        }
+      }
+    },
+    watch:{
+      stylus:{
+        files:['styl/**.styl'],
+        tasks:['stylus:compile']
+      },
+      css:{
+        options:{livereload:true},
+        files:['css/**.css']
+      },
+      html:{
+        options:{livereload:true},
+        files:['**.html']
+      },
+      script:{
+        options:{livereload:true},
+        files:['**.js']
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-stylus');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
   grunt.registerTask('build', []);
 
